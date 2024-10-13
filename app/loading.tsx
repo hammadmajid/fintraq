633667c3326f68dae06061fb73edd927c41bf698
hammadmajid <hammadmajid@proton.me>
2024-10-14 01:10:31 +0500

    add a loading indicator for all routes

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import { Loader2 } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-screen flex-row items-center justify-center gap-2">
+      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      <p>Loading</p>
+    </div>
+  )
+}
