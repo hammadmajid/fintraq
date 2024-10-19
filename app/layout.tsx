b0339ea6715244cc496175019197b1654bf27968
hammadmajid <hammadmajid@proton.me>
2024-10-20 03:08:23 +0500

    extract out Header component & only render on unauthorized routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,11 @@
-import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { Menu, WalletCards } from "lucide-react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Link from "next/link";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Header from "@/components/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -41,6 +31,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body
@@ -64,85 +55,8 @@ export default function RootLayout({
   );
 }
 
-function Header() {
-  interface Link {
-    text: string;
-    href: string;
-  }
 
-  const links: Link[] = [
-    {
-      text: "Features",
-      href: "/#features",
-    },
-    {
-      text: "Pricing",
-      href: "/#pricing",
-    },
-    {
-      text: "Help",
-      href: "/#help",
-    },
-  ];
 
-  return (
-    <header className="p-4 bg-muted/40">
-      <nav className="gap-6 text-lg font-medium flex flex-row items-center justify-between md:gap-5 md:text-sm lg:gap-6">
-        <FintraqLogo />
-        <ul className="hidden md:flex flex-row items-center justify-center">
-          {links.map((link) => {
-            return (
-              <li key={link.text}>
-                <Button variant="link" asChild>
-                  <Link href={link.href}>{link.text}</Link>
-                </Button>
-              </li>
-            );
-          })}
-        </ul>
-        <Sheet>
-          <SheetTrigger className="md:hidden">
-            <Button variant="outline" size="icon" asChild>
-              <Menu />
-            </Button>
-          </SheetTrigger>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>
-                <FintraqLogo />
-              </SheetTitle>
-              <SheetDescription>
-                <ul className="flex flex-col items-start">
-                  {links.map((link) => {
-                    return (
-                      <li key={link.text}>
-                        <Button className="p-0" variant="link" asChild>
-                          <Link href={link.href}>{link.text}</Link>
-                        </Button>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </SheetDescription>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
-      </nav>
-    </header>
-  );
-}
-
-function FintraqLogo() {
-  return (
-    <Link
-      href="/"
-      className="flex items-center gap-2 text-lg font-semibold md:text-base"
-    >
-      <WalletCards className="h-6 w-6" />
-      <span>Fintraq</span>
-    </Link>
-  );
-}
 
 // function Footer() {
 //   return (
