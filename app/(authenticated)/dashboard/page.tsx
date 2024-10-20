285f10d00c46534a7c375e3ace1bea180eda6a76
hammadmajid <hammadmajid@proton.me>
2024-10-21 02:30:25 +0500

    creaet route group for shared layout

---
+++ app/(authenticated)/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { Card, CardContent } from "@/components/ui/card";
+import { SidebarProvider } from "@/components/ui/sidebar"
+
+
+export default function Accounts() {
+  return (
+    <>
+      <main className="w-3/4 mx-auto">
+        <Card>
+          <CardContent className="p-6">
+            <p>Coming soon... </p>
+          </CardContent>
+        </Card>
+      </main>
+    </>
+  );
+}
