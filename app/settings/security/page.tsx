8b25feb9460379f0c897efa2f381ef990c22ce3e
hammadmajid <hammadmajid@proton.me>
2024-10-20 17:14:29 +0500

    create empty placeholder pages for future usage

---
+++ app/settings/security/page.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function Accounts() {
+  return (
+    <main className="w-3/4 mx-auto">
+      <Card>
+        <CardContent className="p-6">
+          <p>Coming soon... </p>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
