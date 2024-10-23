21e5bc78d380cdcdce0126b53203e4eba11c7e92
hammadmajid <hammadmajid@proton.me>
2024-10-24 00:28:04 +0500

    fix path to schema file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     dialect: "postgresql",
-    schema: "./lib/schemas/db.ts",
+    schema: "./lib/db/schema.ts",
     dbCredentials: {
         host: process.env.POSTGRES_HOST!,
         database: process.env.POSTGRES_DATABASE!,
