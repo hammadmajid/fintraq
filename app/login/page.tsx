5c7d206c0b88c97de19e5a0f30311a2f15737de3
Hammad Majid <hammadmajid@proton.me>
2024-12-22 01:54:02 +0500

    feat: redirect user to onboarding step 1 after successful login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,7 +14,7 @@ export default async function Login() {
   const session = await auth();
 
   if (session?.user) {
-    redirect("/u/0/dashboard/");
+    redirect("/onboard/step/1");
   }
 
   return (
