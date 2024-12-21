b2b31598f464e80575ef2dbad1cb7927be2bbec4
Hammad Majid <hammadmajid@proton.me>
2024-12-22 02:00:25 +0500

    feat: add income vs expenses bar chart and expenses pie chart to dashboard

--- app/u/[id]/dashboard/page.tsx
+++ app/u/[id]/dashboard/page.tsx
@@ -1,9 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Plus, FileText } from "lucide-react";
 import { Metadata } from "next";
+import IncomeExpenseBarChart from "./income-expense-bar-chart";
+import ExpensesPieChart from "./expenses-pie-chart";
 
 export const metadata: Metadata = {
   title: "Dashboard",
 };
 
-export default async function Dashboard() {
-  return <div>Dashboard</div>;
+export default function Dashboard() {
+  return (
+    <main>
+      <div className="flex justify-between items-center mb-8">
+        <div>
+          <p className="text-muted-foreground">Balance</p>
+          <h1 className="text-3xl font-bold">$ 1234.56</h1>
+        </div>
+        <div className="space-x-2">
+          <Button variant="outline">
+            <FileText className="w-5" /> Create Report
+          </Button>
+          <Button>
+            <Plus className="w-5"></Plus> New Record
+          </Button>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <IncomeExpenseBarChart />
+        <ExpensesPieChart />
+      </div>
+    </main>
+  );
 }
