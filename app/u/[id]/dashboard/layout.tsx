2e9defb24fcc1b441c4f885f50036e5c3c0fe8d1
Hammad Majid <hammadmajid@proton.me>
2024-12-22 02:20:03 +0500

    feat: add command menu component to dashboard layout

--- app/u/[id]/dashboard/layout.tsx
+++ app/u/[id]/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import UserMenu from "@/components/user-menu";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { CommandMenu } from "@/components/command-menu";
 
 export default async function Layout({
   children,
@@ -24,8 +25,9 @@ export default async function Layout({
         <AppSidebar />
         <div className="flex flex-col w-full">
           <div className="flex justify-between items-center py-2 pr-4">
-            <div className="flex items-center px-4">
+            <div className="flex items-center px-4 gap-4 justify-center">
               <SidebarTrigger />
+              <CommandMenu />
             </div>
             <UserMenu user={session.user} id={id} />
           </div>
