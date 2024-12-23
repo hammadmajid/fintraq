d3c6e8e83aa4f95ef3dbe6adbb9a4833ce1adc47
Hammad Majid <hammadmajid@proton.me>
2024-12-24 03:27:02 +0500

    feat: enhance authentication middleware for improved login redirection

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,15 @@
-export { auth as middleware } from "@/lib/auth"
+import { auth } from "@/lib/auth"
+
+export default auth((req) => {
+    if (!req.auth && req.nextUrl.pathname !== "/login") {
+        const newUrl = new URL("/login", req.nextUrl.origin)
+        return Response.redirect(newUrl)
+    } else if (req.auth && req.nextUrl.pathname === "/login") {
+        const newUrl = new URL("/u/0/dashboard", req.nextUrl.origin)
+        return Response.redirect(newUrl)
+    }
+})
+
+export const config = {
+    matcher: ['/u/:path*', '/onboard/:path*', '/', '/login']
+}
