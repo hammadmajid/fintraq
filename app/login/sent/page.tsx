c5b84c51bd6e01957a9ff69fe25de3b7f5a1b3b2
Hammad Majid <hammadmajid@proton.me>
2024-12-24 10:55:10 +0500

    feat: add Sent page to inform users about magic link email

---
+++ app/login/sent/page.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Check Your Inbox",
+  description: "Please check your inbox for the magic link.",
+};
+
+export default function Sent() {
+  return (
+    <main className="min-h-screen grid place-content-center">
+      <div className="mx-auto grid w-[350px] gap-8">
+        <div>
+          <h1 className="text-3xl font-bold">Check Your Inbox</h1>
+          <p className="text-muted-foreground">
+            We have sent a magic link to your email. Please check your inbox to
+            continue.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
