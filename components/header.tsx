32d0f832271b8a10a2e7394bd6663ef1ac046136
Hammad Majid <hammadmajid@proton.me>
2024-12-24 12:44:04 +0500

    feat: enhance header component with responsive navigation and logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,93 @@
-import Link from 'next/link';
+"use client";
+
 import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu, WalletCards } from "lucide-react";
+import Link from "next/link";
 
 export function Header() {
+  interface Link {
+    text: string;
+    href: string;
+  }
+
+  const links: Link[] = [
+    {
+      text: "Features",
+      href: "/#features",
+    },
+    {
+      text: "Pricing",
+      href: "/#pricing",
+    },
+    {
+      text: "About",
+      href: "/about",
+    },
+  ];
+
   return (
-    <header className="container mx-auto px-4 py-4 flex justify-between items-center">
-      <Link href="/" className="text-2xl font-bold">
-        Fintraq
-      </Link>
-      <nav>
-        <Button variant="ghost" asChild className="mr-2">
-          <Link href="/login">Login</Link>
-        </Button>
-        <Button variant="ghost" asChild>
-          <Link href="/about">About</Link>
-        </Button>
+    <header className="p-4">
+      <nav className="gap-6 text-lg font-medium flex flex-row items-center justify-between md:gap-5 md:text-sm lg:gap-6">
+        <FintraqLogo />
+        <ul className="hidden md:flex flex-row items-center justify-center">
+          {links.map((link) => {
+            return (
+              <li key={link.text}>
+                <Button variant="ghost" asChild>
+                  <Link href={link.href}>{link.text}</Link>
+                </Button>
+              </li>
+            );
+          })}
+        </ul>
+        <Sheet>
+          <SheetTrigger className="md:hidden">
+            <Button variant="ghost" asChild size="icon">
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>
+                <FintraqLogo />
+              </SheetTitle>
+              <SheetDescription asChild>
+                <ul className="flex flex-col items-start">
+                  {links.map((link) => {
+                    return (
+                      <li key={link.text}>
+                        <Button className="p-0" variant="ghost" asChild>
+                          <Link href={link.href}>{link.text}</Link>
+                        </Button>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </nav>
     </header>
   );
 }
 
+function FintraqLogo() {
+  return (
+    <Link
+      href="/"
+      className="flex items-center gap-2 text-lg font-semibold md:text-base"
+    >
+      <WalletCards className="h-6 w-6" />
+      <span>Fintraq</span>
+    </Link>
+  );
+}
