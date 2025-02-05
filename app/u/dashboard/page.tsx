a86bd35d7ca82644c75cb46800bf4569906cf42a
Hammad Majid <hammadmajid@proton.me>
2025-02-05 19:55:53 +0500

    feat: calculate overall total balance in dashboard

--- app/u/dashboard/page.tsx
+++ app/u/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { getRecords } from "@/actions/records";
 import { ExpensesPieChart } from "@/components/dashboard/expenses-pie-chart";
 import { IncomeExpenseBarChart } from "@/components/dashboard/income-expense-bar-chart";
 import { Button } from "@/components/ui/button";
+import { SelectRecord } from "@/drizzle/db/schema";
 import { auth } from "@/lib/auth";
 import { FileText, Plus } from "lucide-react";
 import { Metadata } from "next";
@@ -21,13 +22,14 @@ export default async function Dashboard() {
   const userId = session.user.id;
 
   const records = await getRecords(userId);
+  const totalBalance = calculateTotalBalance(records);
 
   return (
     <main>
       <div className="flex items-center justify-between mb-8">
         <div>
           <p className="text-muted-foreground">Balance</p>
-          <h1 className="text-2xl font-bold">$ 1234.56</h1>
+          <h1 className="text-2xl font-bold">$ {totalBalance}</h1>
         </div>
         <div className="space-x-2">
           <Button variant="outline">
@@ -45,4 +47,18 @@ export default async function Dashboard() {
       </div>
     </main>
   );
+}
+function calculateTotalBalance(records: SelectRecord[]): number {
+  const income = records.reduce((accumulator, current) => {
+    return current.type === "Income" || current.type === "Transfer In"
+      ? accumulator + Number(current.amount)
+      : accumulator;
+  }, 0);
+  const expenses = records.reduce((accumulator, current) => {
+    return current.type === "Expense" || current.type === "Transfer Out"
+      ? accumulator + Number(current.amount)
+      : accumulator;
+  }, 0);
+
+  return income - expenses;
 }
