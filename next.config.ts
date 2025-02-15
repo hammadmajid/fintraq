25a7a157d642e21ab5b01d53c47baccd382424d0
Hammad Majid <hammadmajid@proton.me>
2025-02-15 19:24:35 +0500

    build(breaking): disable experimental features

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["lucide-react"],
-  experimental: {
-    ppr: true,
-    reactCompiler: false,
-  },
 };
 
 export default nextConfig;
