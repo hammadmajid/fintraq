45bed8e2382f8d0ddc957d3e913dfa24495afd1a
Hammad Majid <hammadmajid@proton.me>
2025-02-15 20:25:24 +0500

    feat: update footer to include links to terms and privacy policy

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import { SiGithub } from "react-icons/si";
 
 export function Footer() {
   return (
-    <footer className="container flex items-center justify-between px-4 py-4 mx-auto mt-8 text-sm border-t text-muted-foreground">
-      <div className="flex items-center">
+    <footer className="container flex flex-col sm:flex-row items-center justify-between px-4 py-4 mx-auto mt-8 text-sm border-t text-muted-foreground">
+      <div className="flex items-center space-x-4 mb-2 sm:mb-0">
         <a
           href="https://github.com/hammadmajid/fintraq"
           target="_blank"
@@ -13,6 +14,12 @@ export function Footer() {
           <SiGithub className="w-4 h-4 mr-2" />
           GitHub
         </a>
+        <Link href="/terms" className="hover:text-primary">
+          Terms
+        </Link>
+        <Link href="/privacy" className="hover:text-primary">
+          Privacy
+        </Link>
       </div>
       <div>MIT License</div>
     </footer>
