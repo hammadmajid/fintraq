262ea7491456954f8d58657ab2bcee339afa8f45
Hammad Majid <hammadmajid@proton.me>
2026-04-28 16:03:39 +0500

    feat: setup drizzle with neon db

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+import { defineConfig } from "drizzle-kit";
+
+config({ path: '.env.local' });
+
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+    throw new Error("DATABASE_URL is not defined", {cause: "Undefined"});
+}
+
+export default defineConfig({
+  schema: "./lib/db/schema.ts",
+  out: "./migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: dbUrl,
+  },
+});
