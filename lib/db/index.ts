97806bdd384e6919683ab17998ba83429eb4794c
Hammad Majid <hammadmajid@proton.me>
2026-04-30 00:07:58 +0500

    feat: use t3 env to validate env vars

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,9 +1,4 @@
+import { env } from "@/env"
 import { drizzle } from "drizzle-orm/neon-http"
 
-const dbUrl = process.env.DATABASE_URL
-
-if (!dbUrl) {
-  throw new Error("DATABASE_URL is not defined", { cause: "Undefined" })
-}
-
-export const db = drizzle(dbUrl)
+export const db = drizzle(env.DATABASE_URL)
